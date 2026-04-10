6455430b7b7bbfc5bb500686f01137bb854a9187
Saro <sarobaridoo@gmail.com>
2026-04-10 17:52:21 +0100

    Update CategoryCard.jsx

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -1,33 +1,32 @@
-export type CategoryCardProps = {
-  href: string;
-  imageUrl: string;
-  imageAlt: string;
-  label: string;
-  cardClassName: string;
-  innerDivClassName?: string;
-  labelClassName?: string;
-};
-
-export const CategoryCard = (props: CategoryCardProps) => {
+export const CategoryCard = ({
+  href,
+  imageUrl,
+  imageAlt,
+  label,
+  cardClassName = "",
+  innerDivClassName = "",
+  labelClassName = "",
+}) => {
   return (
     <a
-      href={props.href}
-      className={`relative text-emerald-500 box-border caret-transparent block h-[254px] min-h-[auto] min-w-[auto] text-center w-full px-3 py-4 rounded-xl ${props.cardClassName}`}
+      href={href}
+      className={`relative text-emerald-500 block h-[254px] text-center w-full px-3 py-4 rounded-xl ${cardClassName}`}
     >
       <div
-        className={`relative box-border caret-transparent h-[184px] w-[126px] z-[2] mx-auto before:accent-auto before:bg-zinc-400 before:bg-no-repeat before:box-border before:caret-transparent before:text-emerald-500 before:block before:text-base before:not-italic before:normal-nums before:font-normal before:h-2.5 before:tracking-[normal] before:leading-6 before:list-outside before:list-none before:pointer-events-auto before:absolute before:text-center before:no-underline before:indent-[0px] before:normal-case before:rotate-[-4.000001701562398deg] before:visible before:z-[-1] before:rounded-tr before:border-separate before:left-[3px] before:right-2.5 before:-top-1 before:font-generalsans${props.innerDivClassName ? ` ${props.innerDivClassName}` : ""}`}
+        className={`relative h-[184px] w-[126px] z-[2] mx-auto before:bg-zinc-400 before:block before:absolute before:rotate-[-4deg] before:z-[-1] before:left-[3px] before:right-2.5 before:-top-1 ${innerDivClassName}`}
       >
         <img
-          alt={props.imageAlt}
-          src={props.imageUrl}
+          alt={imageAlt}
+          src={imageUrl}
           sizes="326px"
-          className="relative text-transparent text-[0px] box-border inline h-[184px] leading-[0px] max-w-full w-[126px] z-[2] border border-gray-200 rounded-bl rounded-br rounded-tl rounded-tr border-solid"
+          className="relative h-[184px] w-[126px] max-w-full border border-gray-200 rounded"
         />
       </div>
+
       <div
-        className={`text-zinc-800 text-sm font-semibold box-border caret-transparent tracking-[0.7px] leading-[21px] mt-4${props.labelClassName ? ` ${props.labelClassName}` : ""}`}
+        className={`text-zinc-800 text-sm font-semibold tracking-[0.7px] mt-4 ${labelClassName}`}
       >
-        {props.label}
+        {label}
       </div>
     </a>
   );
