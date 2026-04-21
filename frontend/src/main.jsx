c857deb5ba7ff5c85ab8c5e6a60c10fa938a4f74
Saro <sarobaridoo@gmail.com>
2026-04-21 04:51:09 +0100

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -13,17 +13,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./redux/store";
+
 import { GoogleOAuthProvider } from "@react-oauth/google";
+
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
-          <App />
+          
+          
+          <Elements stripe={stripePromise}>
+            <App />
+          </Elements>
+
         </GoogleOAuthProvider>
       </PersistGate>
     </Provider>
